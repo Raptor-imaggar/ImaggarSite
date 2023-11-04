2d378de0451ff4fa3b7a2f0d63f66b8dfaaf1c9c
thowfickofficial <mdthowfickspeed@gmail.com>
2023-11-04 16:34:05 +0530

    loginPage update

--- src/routing/route.js
+++ src/routing/route.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { Route, Routes as RouterRoutes } from 'react-router-dom';
 import Home from '../pages/home';
 import Careers from '../pages/careers/careers';
+import Login from '../pages/login';
 
 const AppRoutes = () => {
   return (
     <RouterRoutes>
       <Route exact path="/" element={<Home />} />
       <Route path="/careers" element={<Careers />} />
+      <Route path="/login" element={<Login />} />
     </RouterRoutes>
   );
 };

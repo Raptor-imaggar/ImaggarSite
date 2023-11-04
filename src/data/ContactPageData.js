c709421e2b109b52e347fca345abd85af678b184
thowfickofficial <mdthowfickspeed@gmail.com>
2023-11-04 21:26:54 +0530

    contactpage update

---
+++ src/data/ContactPageData.js
@@ -0,0 +1,22 @@
+// ContactPageData.js
+const ContactPageData = {
+    mainTitle: 'Generative AI-Powered Support to Win Lifelong Customers',
+    subtitle: 'With Haptik, you can:',
+    features: [
+        {
+            title: 'Build a Generative AI Assistant in No Time!',
+            description: 'Empower agents with Gen AI-powered chat suggestions, intent detection & other contextual information for effective query resolution.',
+        },
+        // Add more features as needed
+    ],
+    formTitle: 'Request a Demo',
+    firstNamePlaceholder: 'First Name',
+    lastNamePlaceholder: 'Last Name',
+    companyNamePlaceholder: 'Company Name',
+    emailPlaceholder: 'Email',
+    countryCodePlaceholder: 'Select Country Code',
+    phoneNumberPlaceholder: 'Phone Number',
+    submitButtonLabel: 'Submit',
+};
+
+export default ContactPageData;

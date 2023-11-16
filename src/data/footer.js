f393474c91df6350d800b00e02c8ac261abfa367
Raptor-imaggar <131775100+Raptor-imaggar@users.noreply.github.com>
2023-11-16 00:37:45 -0800

    thowfick error fix

---
+++ src/data/footer.js
@@ -0,0 +1,32 @@
+export const footerData = [{
+    
+        "title": "Services",
+        "links": [
+          {"label": "Software Development", "url": "#"},
+          {"label": "Marketing", "url": "#"},
+          {"label": "Branding", "url": "#"},
+          {"label": "Vulnerability Management", "url": "#"}
+        ]
+      },
+       {
+        "title": "Products",
+        "links": [
+          {"label": "C.l.a.r.i.s", "url": "#"},
+          {"label": "S.i.t.e", "url": "#"},
+          {"label": "C.o.r.e", "url": "#"},
+          {"label": "V.o.i.c.e", "url": "#"},
+          {"label": "C.a.r.e", "url": "#"}
+        ]
+      },
+     {
+        "title": "Company",
+        "links": [
+          {"label": "About Us", "url": "#"},
+          {"label": "Our Services", "url": "#"},
+          {"label": "Privacy Policy", "url": "#"},
+          {"label": "Careers", "url": "#"}
+        ]
+      
+    },
+    ]
+  

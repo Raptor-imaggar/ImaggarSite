054ebcf2448cd1ad3e0822f733a749929abed983
thowfickofficial <mdthowfickspeed@gmail.com>
2023-11-17 19:01:44 +0530

    intership form update

---
+++ src/pages/internship/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+  MDBRow,
+  MDBCol,
+  MDBInput,
+  MDBCheckbox,
+  MDBBtn,
+} from 'mdb-react-ui-kit';
+import './internship.css';
+
+export default function App() {
+  return (
+    <div className='internshipform_main'>
+      <form className='internshipform'>
+        <MDBRow className='mb-4'>
+          <MDBCol>
+            <MDBInput id='form6Example1' label='First name' />
+          </MDBCol>
+          <MDBCol>
+            <MDBInput id='form6Example2' label='Last name' />
+          </MDBCol>
+        </MDBRow>
+
+        <MDBInput wrapperClass='mb-4' id='form6Example3' label='Company name' />
+        <MDBInput wrapperClass='mb-4' id='form6Example4' label='Address' />
+        <MDBInput wrapperClass='mb-4' type='email' id='form6Example5' label='Email' />
+        <MDBInput wrapperClass='mb-4' type='tel' id='form6Example6' label='Phone' />
+
+        <div className='mb-4'>
+          <label htmlFor='selectItem' className='form-label'>
+            Select Item
+          </label>
+          <select className='form-select' id='selectItem'>
+            <option value='1'>Option 1</option>
+            <option value='2'>Option 2</option>
+            <option value='3'>Option 3</option>
+          </select>
+        </div>
+
+        <div className='mb-4'>
+          <label htmlFor='resumeInput' className='form-label'>
+            Browse Resume
+          </label>
+          <input type='file' className='form-control' id='resumeInput' />
+        </div>
+
+        <MDBCheckbox
+          wrapperClass='d-flex justify-content-center mb-4'
+          id='form6Example8'
+          label='I have read and agree to the terms'
+          defaultChecked
+        />
+
+        <MDBBtn className='mb-4' type='submit' block>
+          Submit
+        </MDBBtn>
+      </form>
+    </div>
+  );
+}
--- src/routing/route.js
+++ src/routing/route.js
@@ -4,6 +4,7 @@ import { Route, Routes as RouterRoutes } from 'react-router-dom';
 import Home from '../pages/home';
 import Contact from '../pages/GetAdemo/';
 import Login from '../pages/login';
+import Internship from '../pages/internship';
 
 const AppRoutes = () => {
   return (
@@ -15,6 +16,7 @@ const AppRoutes = () => {
       <Route path="/services" element={<Contact />} />
       <Route path="/products" element={<Contact />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/internship" element={<Internship />} />
       <Route exact path="*" element={<Home />} />
     </RouterRoutes>
   );
